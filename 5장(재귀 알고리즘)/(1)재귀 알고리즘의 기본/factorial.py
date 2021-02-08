458cf56476a06d0e9f2d7f4ca96a5d20a8514017
JaeWon Baek <63109802+bjw1622@users.noreply.github.com>
2021-02-08 21:53:33 +0900

    Add files via upload

---
+++ 5장(재귀 알고리즘)/(1)재귀 알고리즘의 기본/factorial.py
@@ -0,0 +1,9 @@
+def factorial(value):
+    if value == 0:
+        return 1
+    else:
+        return value * factorial(value-1)
+
+if __name__ == '__main__':
+    n = int(input('출력할 팩토리얼값을 입력하세요.:'))
+    print(f'{n}의 팩토리얼은 {factorial(n)}입니다.')
