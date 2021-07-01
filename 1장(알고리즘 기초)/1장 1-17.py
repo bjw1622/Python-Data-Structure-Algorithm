86fc4fb458c33d71cdef5c7c1278384b23b23144
JaeWon Baek <63109802+bjw1622@users.noreply.github.com>
2021-07-01 10:24:59 +0900

    Add files via upload

--- 1장(알고리즘 기초)/1장 1-17.py
+++ 1장(알고리즘 기초)/1장 1-17.py
@@ -1,9 +1,8 @@
 area = int(input("직사각형의 넓이를 입력하세요:"))
 
-for i in range(1,area+1):
-    for j in range(1,area+1):
-        if i > j:
-            continue
-        else:
-            if i * j == area:
-                print(f"{i}x{j}")
+for i in range(1,(area//2)+1):
+    #반복 막는 조건문
+    if i * i >area :
+        break
+    elif i * (area // i) == area:
+        print(f"{i} x {area//i}")
