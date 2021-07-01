43940a72d175c3b5882e3582a3bb381bf919e5ab
JaeWon Baek <63109802+bjw1622@users.noreply.github.com>
2021-07-01 09:32:47 +0900

    Add files via upload

---
+++ 1장(알고리즘 기초)/1장 1-2.py
@@ -0,0 +1,10 @@
+# 1-1 함수화하여 진행
+
+def max_return(a,b,c):
+    max_data = max(a,b,c)
+    return max_data
+
+
+print(f"{max_return(3,2,1)}")
+print(f"{max_return(3,2,2)}")
+print(f"{max_return(100,100,101)}")
