8d91713925c76813bb94a9c7a07786aef0e05db6
JaeWon Baek <63109802+bjw1622@users.noreply.github.com>
2021-07-01 09:25:12 +0900

    Add files via upload

--- 1장(알고리즘 기초)/1장 1-1.py
+++ 1장(알고리즘 기초)/1장 1-1.py
@@ -1,11 +1,23 @@
-def max3(a,b,c):
-    max = a
-    if (b > max):
-        max = b
-    if( c > max):
-        max = c
-    return max
+print("세 정수의 최댓값을 구합니다.")
 
-print(f"최댓값은 {max3(4,5,6)}입니다.")
+a = int(input("정수 a의 값을 입력하세요:"))
+b = int(input("정수 b의 값을 입력하세요:"))
+c = int(input("정수 c의 값을 입력하세요:"))
 
+max_data = max(a,b,c)
 
+print(f"최댓값은 {max_data}입니다.")
+
+"""
+책 예제
+max = a
+if b > max:
+    max = b
+if c > max:
+    max = c
+    
+순서대로 처리되는 구조를 순차구조.
+
+조건식으로 평가한 결과에 따라 프로그램의 실행 흐름이 변경되는건 선택 구조
+
+"""
