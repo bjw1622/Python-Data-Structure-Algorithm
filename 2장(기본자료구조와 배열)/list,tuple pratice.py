ec540c27060969633a04ae2306290fa9e83fb15f
JaeWon Baek <63109802+bjw1622@users.noreply.github.com>
2021-07-02 09:19:29 +0900

    Add files via upload

---
+++ 2장(기본자료구조와 배열)/list,tuple pratice.py
@@ -0,0 +1,17 @@
+list01 = list([1,2,3])
+list02 = list(range(1,5))
+list03 = list(range(1,5,3))
+list04 = list(["A","B",'C'])
+
+
+tuple01 = tuple((1,))
+tuple02 = tuple((1,23,231))
+
+
+print(list01)
+print(list02)
+print(list03)
+print(list04)
+
+print(tuple01)
+print(tuple02)
